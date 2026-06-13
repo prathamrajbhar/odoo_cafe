6a356a720fd614fe8e59c431fc3c39e1cf949b1c
prathamrajbhar <pratham.rajbhar987@gmail.com>
2026-06-13 11:31:09 +0530

    workspace created

---
+++ src/lib/prisma.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const connectionString = process.env.DATABASE_URL  as string;
+
+const adapter = new PrismaPg(connectionString);
+
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
+};
+
+export const prisma =
+  globalForPrisma.prisma ?? new PrismaClient({adapter});
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
